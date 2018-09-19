d42403c4f5bbbb4f99202fd522a15a8e33b1ece9
liujinxing <jinxing.liu@verystar.cn>
2018-09-19 11:49:36 +0800

    使用 gulp 压缩文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify');
 
 gulp.task('default', function () {
     // 将你的默认的任务代码放在这
-    gulp.src('public/js/layer/layer.js')
-        .pipe(uglify())
-        .pipe(minify())
-        .pipe(gulp.dest('public/js/layer/layer.min.js'));
+    gulp.src('public/js/meTables/*.js')
+        .pipe(minify({
+            ext: {
+                min: ".min.js"
+            }
+        }))
+        .pipe(gulp.dest('public/js'));
 });
 
 
