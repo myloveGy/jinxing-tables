569245b970c1e53841ad6a6611c15c9210f05431
刘金星 <821901008@qq.com>
2019-06-28 22:44:26 +0800

    refactor: 资源目录修改

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
-var gulp = require('gulp');
-var minify = require('gulp-minify');
+var gulp = require('gulp')
+var minify = require('gulp-minify')
 
 gulp.task('default', function () {
-    // 将你的默认的任务代码放在这
-    gulp.src('public/js/meTables/!(*.min.js)')
-        .pipe(minify({
-            ext: {
-                min: ".min.js"
-            }
-        }))
-        .pipe(gulp.dest('./'));
-});
+  // 将你的默认的任务代码放在这
+  gulp.src('public/js/meTables/!(*.min.js)')
+    .pipe(minify({
+      ext: {
+        min: '.min.js',
+      },
+    }))
+    .pipe(gulp.dest('./dist'))
+})
 
 
